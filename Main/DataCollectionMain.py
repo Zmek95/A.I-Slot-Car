95b1bcf8dd4a442c43120849d23c2126a6e23f28
Ziyad Mekhemer <zmek95@gmail.com>
2020-11-22 20:39:33 -0500

    Update DataCollectionMain.py

--- Main/DataCollectionMain.py
+++ Main/DataCollectionMain.py
@@ -1,13 +1,11 @@
-from PWM import forward, stop
+from pwmGenerate import forward, stop
 from SensorRecord import sensor_record
 
 # Get user speed and test time
 speed = int(input("Enter the desired duty cycle 0 - 100"))
 sensor_time = int(input("Enter the desired sensor read time in seconds"))
-# First set speed of motor to 50%
+# First set the speed of the motor
 forward(speed)
-
 # Record readings from sensor
 sensor_record(sensor_time)
 stop()
-
